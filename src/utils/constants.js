5f227f120f521194f3e69c656f86f8f6eafaa4e5
ykisialiou <hiluro@gmail.com>
2019-03-17 23:24:33 +0300

    Correct code style

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -55,73 +55,73 @@ constants.SOKOL = 'sokol'
 constants.CORE = 'core'
 constants.DAI = 'dai'
 
-function parseVcap (chartName) {
+function parseVcap(chartName) {
+  let vcap = process.env.VCAP_SERVICES
+  debugger
+  var vcapJson = JSON.parse(vcap)[chartName][0].credentials.services
 
-  var vcap = process.env.VCAP_SERVICES
-  var vcapJson = JSON.parse(vcap)[chartName][0]['credentials']['services']
-  
-  var properties = {} 
+  var properties = {}
   var services = []
-  
-  vcapJson.forEach(function(service){
 
-  var ports = service['spec']['ports']
-  var found = ports.find(function(el){
-    return el.name == "rpc"
+  vcapJson.forEach(function(service) {
+    var ports = service['spec']['ports']
+    var found = ports.find(function(el) {
+      return el.name === 'rpc'
+    })
+
+    properties = {
+      name: service['name'],
+      IP: service['spec']['clusterIP'],
+      NodePort: found.nodePort
+    }
+    services.push(properties)
   })
-  	
-  properties = { "name": service['name'], "IP": service['spec']['clusterIP'], "NodePort": found.nodePort }
-  services.push(properties)	
-
-  });
-
   let NETWORKS = {
-      '77': {
-        NAME: 'Sokol',
-        RPC: 'https://sokol.poa.network',
-        BRANCH: constants.SOKOL,
-        TESTNET: true
-      }
+    '77': {
+      NAME: 'Sokol',
+      RPC: 'https://sokol.poa.network',
+      BRANCH: constants.SOKOL,
+      TESTNET: true
+    }
   }
-  
-  services=merge(NETWORKS,services)
-  console.log(services)
-  return services	
+
+  services = merge(NETWORKS, services)
+  return services
 }
 
-function merge(x,y) {
-    
-    let newObject = {}
-
-    var i = 0
-
-for (const key in x) {
+function merge(x, y) {
+  let newObject = {}
+  var i = 0
+  for (const key in x) {
     if (x.hasOwnProperty(key)) {
-        const element = x[key]
-        let arrElement = y[i]
+      const element = x[key]
+      let arrElement = y[i]
 
-        if (!arrElement)
-            break
-        
-        element.NAME = arrElement.name
-        element.RPC = `${arrElement.IP}:${arrElement.NodePort}`
+      if (!arrElement) break
 
-        newObject[key] = element
+      element.NAME = arrElement.name
+      element.RPC = `${arrElement.IP}:${arrElement.NodePort}`
+
+      newObject[key] = element
     }
     i++
-}
-    
-    if (y.length > i)
-    {
-        y.forEach(element => {
-            if (!newObject[element.key])
-                newObject[element.key] = { name: element.name, RPC: `${element.IP}:${element.NodePort}`, BRANCH: "core", TESTNET:"false"}
-        });
-    }
-    return newObject
+  }
+
+  if (y.length > i) {
+    y.forEach(element => {
+      if (!newObject[element.key])
+        newObject[element.key] = {
+          name: element.name,
+          RPC: `${element.IP}:${element.NodePort}`,
+          BRANCH: 'core',
+          TESTNET: 'false'
+        }
+    })
+  }
+  return newObject
 }
 
-constants.NETWORKS = parseVcap('poanode');
+constants.NETWORKS = parseVcap('poanode')
 
 module.exports = {
   constants
