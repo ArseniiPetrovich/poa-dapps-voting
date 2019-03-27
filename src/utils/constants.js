eba7dc6b794ad6ddd1549b8fa8630f0319c339d1
ykisialiou <hiluro@gmail.com>
2019-03-27 10:20:56 +0300

    Read vcap env var

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -55,73 +55,51 @@ constants.SOKOL = 'sokol'
 constants.CORE = 'core'
 constants.DAI = 'dai'
 
-function parseVcap(chartName) {
-  let vcap = process.env.VCAP_SERVICES
-  debugger
-  var vcapJson = JSON.parse(vcap)[chartName][0].credentials.services
-
-  var properties = {}
-  var services = []
-
-  vcapJson.forEach(function(service) {
-    var ports = service['spec']['ports']
-    var found = ports.find(function(el) {
-      return el.name === 'rpc'
-    })
-
-    properties = {
-      name: service['name'],
-      IP: service['spec']['clusterIP'],
-      NodePort: found.nodePort
-    }
-    services.push(properties)
-  })
-  let NETWORKS = {
-    '77': {
-      NAME: 'Sokol',
-      RPC: 'https://sokol.poa.network',
-      BRANCH: constants.SOKOL,
-      TESTNET: true
-    }
+let vcap = process.env.VCAP_SERVICES
+let vcapJson = JSON.parse(vcap)
+let vcapKeys = Object.keys(JSON.parse(vcap))
+// Search throught VCAP_SERVICES for service with label
+// for now we suppose that only one per chart kibosh service
+// binded to app. TODO: make it possible to bind several service
+// instances from one sourse
+const filtered = Object.keys(vcapJson)
+  .filter(key => vcapJson[key][0].label == 'poanode')
+  .reduce((obj, key) => {
+    obj = vcapJson[key][0]
+    return obj
+  }, {})
+const k8sSecrets = filtered.credentials.secrets
+// Kubernetes services object, gathered from VCAP
+const k8sSvcs = filtered.credentials.services
+// Filter services. For now we need tcp services, related to label poanode
+const k8sTCPSvcs = k8sSvcs.filter(service => service.name.includes('tcp') && service.name.includes('poanode'))
+let newObj = {}
+k8sTCPSvcs.forEach(function(service) {
+  // TODO: check service spec
+  let conf = {}
+  let confValue = {}
+  // Filter ports
+  let rpcPort = service.spec.ports.filter(port => port.name == 'rpc')[0]
+  // Filter secrets
+  let namePrefixArray = service.name.split('-')
+  let namePrefix = namePrefixArray[namePrefixArray.length - 1]
+  let filteredConfValues = k8sSecrets.filter(secret => secret.name.includes(namePrefix))
+  if (filteredConfValues.length == 1) {
+    confValue = filteredConfValues[0]
   }
-
-  services = merge(NETWORKS, services)
-  return services
-}
-
-function merge(x, y) {
-  let newObject = {}
-  var i = 0
-  for (const key in x) {
-    if (x.hasOwnProperty(key)) {
-      const element = x[key]
-      let arrElement = y[i]
-
-      if (!arrElement) break
-
-      element.NAME = arrElement.name
-      element.RPC = `${arrElement.IP}:${arrElement.NodePort}`
-
-      newObject[key] = element
-    }
-    i++
+  let externalIP = service.status.loadBalancer.ingress
+  if (externalIP.length == 1) {
+    conf.RPC = 'http://' + externalIP[0].ip + ':' + rpcPort.port
   }
+  conf.BRANCH = confValue.data.branch
+  conf.TESTNET = confValue.data.testnet.replace(/['"]+/g, '')
+  conf.NAME = service.name
+  newObj[confValue.data.chainid.replace(/['"]+/g, '')] = conf
+})
 
-  if (y.length > i) {
-    y.forEach(element => {
-      if (!newObject[element.key])
-        newObject[element.key] = {
-          name: element.name,
-          RPC: `${element.IP}:${element.NodePort}`,
-          BRANCH: 'core',
-          TESTNET: 'false'
-        }
-    })
-  }
-  return newObject
-}
-
-constants.NETWORKS = parseVcap('poanode')
+//constants.NETWORKS = parseVcap('poanode')
+debugger
+constants.NETWORKS = newObj
 
 module.exports = {
   constants
